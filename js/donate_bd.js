5df132f4cf2db46252c12aba8de448fb4ce05025
Mansib101 <mansibuddin101@gmail.com>
2024-09-25 20:09:45 +0600

    done with transaction page

--- js/donate_bd.js
+++ js/donate_bd.js
@@ -1,15 +1,34 @@
 // Noakhali
 document.getElementById('nkh-btn')
 .addEventListener('click', function(){
-    donation_function('nkh-total', 'nkh-input');
+    donation_function('nkh-total', 'nkh-input', 'Flood at Noakhalo', 'my_modal_1');
 })
 // Feni
 document.getElementById('feni-btn')
 .addEventListener('click', function(){
-    donation_function('feni-total', 'feni-input');
+    donation_function('feni-total', 'feni-input', 'Flood relief in Feni', 'my_modal_2');
 })
 // Quota
 document.getElementById('quota-btn')
 .addEventListener('click', function(){
-    donation_function('quota-total', 'quota-input');
+    donation_function('quota-total', 'quota-input', 'Aid for injured in Quota movement', 'my_modal_3');
+})
+
+// Donation and History page interchange btns
+document.getElementById('history-page-btn')
+.addEventListener('click', function(){
+    document.getElementById('history-page-btn').classList.add('bg-green');
+    document.getElementById('card-page-btn').classList.remove('bg-green');
+    document.getElementById('card-section').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('main-section').classList.add('flex-col');       
+})
+
+document.getElementById('card-page-btn')
+.addEventListener('click', function(){
+    document.getElementById('card-page-btn').classList.add('bg-green');
+    document.getElementById('history-page-btn').classList.remove('bg-green');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('card-section').classList.remove('hidden');
+    document.getElementById('main-section').classList.remove('flex-col');       
 })
--- js/utils.js
+++ js/utils.js
@@ -4,7 +4,7 @@ function input_value(id){
     return amount;
 }
 
-function donation_function(card_id, input_id){
+function donation_function(card_id, input_id, cause_txt, modal_id){
     const d_money = parseFloat(input_value(input_id));
     if (isNaN(d_money) || d_money<0){
         alert("Invalid Input. Please type in proper amount");
@@ -16,11 +16,23 @@ function donation_function(card_id, input_id){
             alert('You dont have sufficient balance!');
         }
         else{
+            // upadating total values
             const nkh_total = parseFloat(document.getElementById(card_id).innerText);
             const new_nkh_total = nkh_total + d_money;
             document.getElementById('total-amount').innerText = new_total;            
             document.getElementById(card_id).innerText = new_nkh_total;
+            // creating transaction history
+            const transaction_div = document.createElement('div');
+            transaction_div.classList.add("border-2", "rounded-xl", "mt-8", "p-6");
+            transaction_div.innerText = `${d_money} BDT is Donated for ${cause_txt}, Bangladesh
+
+            ${Date()}
+            `
+            document.getElementById('history-section').appendChild(transaction_div);
+            // Showing pop-up modal
+            document.getElementById(modal_id).showModal();
         }
     }
 
 }
+
